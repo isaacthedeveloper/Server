812c65a703406fcf7ca8a43f57f03675b84145bd
Isaac Ballas <isaacthedeveloper@gmail.com>
2019-12-06 15:28:20 -0500

    Initial Commit

---
+++ Server/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Server
+//
+//  Created by Isaac Ballas on 2019-12-06.
+//  Copyright © 2019 Isaac Ballas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
